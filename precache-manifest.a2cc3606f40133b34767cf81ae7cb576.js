d2e1c33bad592c026c470843d5368bcecd29181e
unknown <monika.pikuzinska01@gmai.com>
2020-06-26 13:02:20 +0200

    Updates

---
+++ precache-manifest.a2cc3606f40133b34767cf81ae7cb576.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3f06a1cd855f5888ffc33dea2e4629b",
+    "url": "/book-marathon-app/index.html"
+  },
+  {
+    "revision": "e0a1e5ae9be8e439b2fc",
+    "url": "/book-marathon-app/static/css/main.5633a6c5.chunk.css"
+  },
+  {
+    "revision": "f15dd190991468dd994d",
+    "url": "/book-marathon-app/static/js/2.a29a13c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/book-marathon-app/static/js/2.a29a13c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0a1e5ae9be8e439b2fc",
+    "url": "/book-marathon-app/static/js/main.4fc2c3a2.chunk.js"
+  },
+  {
+    "revision": "93acb89b95e41eaf20f2",
+    "url": "/book-marathon-app/static/js/runtime-main.17c5a2da.js"
+  },
+  {
+    "revision": "c4fdb2a437152c27b3923333005f612b",
+    "url": "/book-marathon-app/static/media/remove.c4fdb2a4.svg"
+  }
+]);
